9ce0eb6d9bc2562a24f5dfd2cc270b3165ff225e
Muaaz Tariq CP <muaaz.tariq@cloudpacer.com>
2025-03-11 17:06:39 +0500

    add dist

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DbModule = void 0;
+var db_module_1 = require("./db.module");
+Object.defineProperty(exports, "DbModule", { enumerable: true, get: function () { return db_module_1.DbModule; } });
+//# sourceMappingURL=index.js.map
